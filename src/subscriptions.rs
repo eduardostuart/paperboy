15621d90eb1f7f7b1bfb3ee6c8023eaf0f4af52f
Eduardo Stuart <hi@s.tuart.me>
2021-11-18 07:16:02 +0100

    simplify tests

--- src/subscriptions.rs
+++ src/subscriptions.rs
@@ -31,9 +31,11 @@ https://b/feed"#,
 
         test_util::run(|| {
             let subs = load_from_file(&file_path);
-            assert_eq!(2, subs.clone().len());
-            assert_eq!(subs.clone().into_iter().nth(0).unwrap(), "https://a/feed");
-            assert_eq!(subs.into_iter().nth(1).unwrap(), "https://b/feed");
+            assert_eq!(2, subs.len());
+            assert_eq!(
+                vec!["https://a/feed".to_string(), "https://b/feed".to_string(),],
+                subs
+            );
         });
 
         fs::remove_dir_all(path).unwrap();
@@ -52,12 +54,16 @@ https://c/feed"#,
         );
 
         test_util::run(|| {
-            let read_line = |vec: Vec<String>, row: usize| vec.into_iter().nth(row);
             let subs = load_from_file(&file_path);
-            assert_eq!(3, subs.clone().len());
-            assert_eq!(read_line(subs.clone(), 0).unwrap(), "https://a/feed");
-            assert_eq!(read_line(subs.clone(), 1).unwrap(), "https://b/feed");
-            assert_eq!(read_line(subs, 2).unwrap(), "https://c/feed");
+            assert_eq!(3, subs.len());
+            assert_eq!(
+                vec![
+                    "https://a/feed".to_string(),
+                    "https://b/feed".to_string(),
+                    "https://c/feed".to_string()
+                ],
+                subs
+            );
         });
 
         fs::remove_dir_all(path).unwrap();
@@ -75,9 +81,11 @@ https://c/feed
 
         test_util::run(|| {
             let subs = load_from_file(&file_path);
-            assert_eq!(2, subs.clone().len());
-            assert_eq!(subs.clone().into_iter().nth(0).unwrap(), "https://a/feed");
-            assert_eq!(subs.into_iter().nth(1).unwrap(), "https://c/feed");
+            assert_eq!(2, subs.len());
+            assert_eq!(
+                vec!["https://a/feed".to_string(), "https://c/feed".to_string()],
+                subs
+            );
         });
 
         fs::remove_dir_all(path).unwrap();
