a920eabbcfe2c20c5f336bf5430fe80b1446dd3b
Eduardo Stuart <hi@s.tuart.me>
2022-02-15 23:20:26 +0100

    refactor: simplify main

--- paperboy-cli/src/main.rs
+++ paperboy-cli/src/main.rs
@@ -18,49 +18,51 @@ async fn main() -> Result<()> {
 
     let matches = Args::parse();
 
-    let verbose = matches.verbose;
-
     match matches.commands {
         // Deliver RSS by email
         Commands::Deliver {
             email,
             template_file,
         } => {
-            let smtp_from = get_env_key("SMTP_FROM", "SMTP is not defined");
-            let smtp_host = get_env_key("SMTP_HOST", "SMTP host is not defined");
-            let smtp_password = get_env_key("SMTP_PASSWORD", "SMTP password is not defined");
-            let smtp_username = get_env_key("SMTP_USERNAME", "SMTP username is not defined");
-            let smtp_port = match option_env!("SMTP_PORT") {
-                Some(p) => p.parse::<u16>().unwrap(),
-                None => 25,
-            };
-
-            let config = MailConfig {
-                smtp_from: &smtp_from,
-                smtp_host: &smtp_host,
-                smtp_password: &smtp_password,
-                smtp_username: &smtp_username,
-                smtp_port: &smtp_port,
-            };
-
-            let template = match template_file {
-                Some(v) => v,
-                None => "emails/daily_email.hbs".to_string(),
-            };
-
-            let wants_verbose = verbose.is_positive();
-
-            let result = Deliver::new(SUBSCRIPTIONS_FILE, &template, config)
-                .handle(&email, wants_verbose)
-                .await?;
-
-            println!("Result: {:?}", result.message);
-
-            if result.errors.is_some() && wants_verbose {
-                println!("Errors: {:?}", result.errors.unwrap());
-            }
+            deliver_rss_by_email(email, template_file, matches.verbose.is_positive()).await?;
         }
     };
+
+    Ok(())
+}
+
+async fn deliver_rss_by_email(
+    email: String,
+    template_file: Option<String>,
+    verbose: bool,
+) -> Result<()> {
+    let smtp_port = match option_env!("SMTP_PORT") {
+        Some(p) => p.parse::<u16>().unwrap(),
+        None => 25,
+    };
+
+    let config = MailConfig {
+        smtp_from: &get_env_key("SMTP_FROM", "SMTP is not defined"),
+        smtp_host: &get_env_key("SMTP_HOST", "SMTP host is not defined"),
+        smtp_password: &get_env_key("SMTP_PASSWORD", "SMTP password is not defined"),
+        smtp_username: &get_env_key("SMTP_USERNAME", "SMTP username is not defined"),
+        smtp_port: &smtp_port,
+    };
+
+    let template = match template_file {
+        Some(v) => v,
+        None => "emails/daily_email.hbs".to_string(),
+    };
+
+    let result = Deliver::new(SUBSCRIPTIONS_FILE, &template, config)
+        .handle(&email, verbose)
+        .await?;
+
+    println!("Result: {:?}", result.message);
+
+    if result.errors.is_some() && verbose {
+        println!("Errors: {:#?}", result.errors.unwrap());
+    }
 
     Ok(())
 }
